56ffe7df66b29a5017629ac75e4979ff15b8d4bf
Ruth Enjelette <ruth.enjelette@gmail.com>
2019-01-07 09:39:29 +0800

    Set rootPage to LoginPage
    from HomePage to LoginPage

--- IonicApps/src/app/app.component.ts
+++ IonicApps/src/app/app.component.ts
@@ -19,7 +19,7 @@ import { PartnershipsPage } from '../pages/partnerships/partnerships';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = LoginPage;
 
   pages: Array<{title: string, component: any}>;
 
