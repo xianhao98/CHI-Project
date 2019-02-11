372006f42953d2c07938729d468006a82cbbd2ae
xianhao98 <40017627+xianhao98@users.noreply.github.com>
2019-02-12 01:54:37 +0800

    edit profile page, event details(hard coded) and speakers(hard-coded)

--- IonicApps/src/pages/event-details/event-details.ts
+++ IonicApps/src/pages/event-details/event-details.ts
@@ -1,23 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { database } from 'firebase';
 import { Observable } from 'rxjs-compat/Observable';
-
+import { HomePage } from '../home/home';
+import { eventDetails } from '../../models/eventdetails';
 /**
  * Generated class for the EventDetailsPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
+var data =[];
 @IonicPage()
 @Component({
   selector: 'page-event-details',
   templateUrl: 'event-details.html',
 })
-export class EventDetailsPage {
-
+export class EventDetailsPage implements OnInit{
+  eventdetails: eventDetails[];
   eventid: string;
 
   // event = {} as Event;
@@ -34,16 +35,27 @@ export class EventDetailsPage {
 
     this.getDetails();
   }
-
+  ngOnInit(){
+    setTimeout(() => {
+      console.log("in ngOnInit method");
+      console.log(data);
+      console.log(data.length);
+      this.eventdetails = [
+        new eventDetails(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7])
+      ];
+    }, 2100);
+  }
   getDetails() {
-    this.db.collection("events").get().subscribe((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        this.db.collection("events").doc(doc.id).collection("speakers").get().subscribe((querySnapshot) => {
-          querySnapshot.forEach((docs) => {
-            console.log('Current Event ID: ', doc.id);
-          })
-        })
-      })
+    this.db.collection("events").doc("e001").get().subscribe(doc => {
+      console.log(doc.data());
+      data.push(doc.data().eventDateEnd);
+      data.push(doc.data().eventDateStart);
+      data.push(doc.data().eventDesc);
+      data.push(doc.data().eventName);
+      data.push(doc.data().eventType);
+      data.push(doc.data().eventVenue);
+      data.push(doc.data().slotsTotal);
+      data.push(doc.data().imgURL);
     })
   }
 
